bfb30f2d8d86783d182fc64d1e888118b77aebfa
PengShaw <hhxp9106@gmail.com>
2022-06-10 16:03:42 +0800

    fix(src/block/block.tsx): change block prop of block component
    change block prop of block component from BlockObjectResponse to PartialBlockObjectResponse |
    BlockObjectResponse. Thus, results of Notion ListBlockChildrenResponse Api can be used directly to
    Block Compomnent.

--- src/Block/Block.tsx
+++ src/Block/Block.tsx
@@ -5,34 +5,44 @@ import List from "../components/List/List";
 import Todo from "../components/Todo/Todo";
 import Code from "../components/Code/Code";
 
-import { BlockObjectResponse } from "../notion.types";
+import { BlockObjectResponse, PartialBlockObjectResponse } from "../notion.types";
 
 export interface Props {
-    block: BlockObjectResponse;
+    block: PartialBlockObjectResponse | BlockObjectResponse;
     strict?: boolean;
 };
 
 const Block: React.FC<Props> = ({ block, strict }) => {
-    switch (block.type) {
-        case "paragraph":
-            return <Paragraph block={block} strict={strict} />;
-        case "heading_1":
-        case "heading_2":
-        case "heading_3":
-            return <Heading block={block} strict={strict} />;
-        case "bulleted_list_item":
-        case "numbered_list_item":
-            return <List block={block} strict={strict} />;
-        case "to_do":
-            return <Todo block={block} strict={strict} />;
-        case "code":
-            return <Code block={block} strict={strict} />;
-        default:
-            if (strict === true) {
-                throw new Error(`not implement: type ${block.type}`);
-            }
-            return (<div data-testid='block-component'></div>);
+    if (block.hasOwnProperty('type')) {
+        const blockObject = block as BlockObjectResponse;
+        switch (blockObject.type) {
+            case "paragraph":
+                return <Paragraph block={blockObject} strict={strict} />;
+            case "heading_1":
+            case "heading_2":
+            case "heading_3":
+                return <Heading block={blockObject} strict={strict} />;
+            case "bulleted_list_item":
+            case "numbered_list_item":
+                return <List block={blockObject} strict={strict} />;
+            case "to_do":
+                return <Todo block={blockObject} strict={strict} />;
+            case "code":
+                return <Code block={blockObject} strict={strict} />;
+            default:
+                if (strict === true) {
+                    throw new Error(`not implement: type ${blockObject.type}`);
+                }
+                return (<div data-testid='block-component'></div>);
+        }
+    } else {
+        if (strict === true) {
+            throw new Error(`not implement: type PartialBlockObjectResponse`);
+        }
+        return (<div data-testid='block-component'></div>);
     }
+
+
 };
 
 export default Block;
